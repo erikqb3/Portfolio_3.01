7c1b54aa80960b96d4185c0d9d100a1f1dbedc4f
erikqbirch <erikqbirch@gmail.com>
2023-02-21 11:03:29 -0700

    picUpdates

--- assets/resources/previewDB.js
+++ assets/resources/previewDB.js
@@ -2,24 +2,24 @@ export const previewDB = {
   "project01": {
     "id" : "01",
     "name": "Creative<br>Keturah",
-    "thumbnailPath": "assets/resources/imgs/previewPics/CreativeKeturah/KCT.png",
+    "thumbnailPath": "assets/resources/imgs/previewPics/CreativeKeturah/CKthb.png",
     "preview" : {
       "previewPath": "pages/preview.html?id=01",
       "sitePath":"https://erikqbirch.github.io/CreativeKeturah/",
       "year":2022,
-      "carouselImgs" :["assets/resources/imgs/previewPics/CreativeKeturah/KC1.png","assets/resources/imgs/previewPics/CreativeKeturah/KC2.png","assets/resources/imgs/previewPics/CreativeKeturah/KC3.png","assets/resources/imgs/previewPics/CreativeKeturah/KC4.png","assets/resources/imgs/previewPics/CreativeKeturah/KC5.png"],
+      "carouselImgs" :["assets/resources/imgs/previewPics/CreativeKeturah/CK1.webp","assets/resources/imgs/previewPics/CreativeKeturah/CK2.webp","assets/resources/imgs/previewPics/CreativeKeturah/CK3.webp","assets/resources/imgs/previewPics/CreativeKeturah/CK4.webp","assets/resources/imgs/previewPics/CreativeKeturah/CK5.webp"],
       "description" : "With permission from Keturah Williams herself, a vast majority of these amazing illustrations were pulled from her instagram and given place in an website suitable for their elegance. With one or two interviews over zoom, titles and descriptions were also given to Keturah's great works, but the interpration of her masterpieces is truly up to the viewers."
     }
   },
   "project02": {
     "id" : "02",
     "name": "Sapien Designs<br>Multi-Media",
-    "thumbnailPath": "assets/resources/imgs/placeholder2.png",
+    "thumbnailPath": "assets/resources/imgs/previewPics/SapienDesigns/SDthb.png",
     "preview" : {
       "previewPath": "pages/preview.html?id=02",
       "sitePath":"https://seo.sapiendesigns.com/",
       "year":2022,
-      "carouselImgs" :["assets/resources/imgs/placeholder.jpg","assets/resources/imgs/placeholder.jpg","assets/resources/imgs/placeholder.jpg","assets/resources/imgs/placeholder.jpg","assets/resources/imgs/placeholder.jpg"],
+      "carouselImgs" :["assets/resources/imgs/previewPics/SapienDesigns/SD1.png","assets/resources/imgs/previewPics/SapienDesigns/SD2.png","assets/resources/imgs/previewPics/SapienDesigns/SD3.png","assets/resources/imgs/previewPics/SapienDesigns/SD4.png","assets/resources/imgs/previewPics/SapienDesigns/SD5.png"],
       "description" : "Sapien Designs was the start up company I did my college internship for and is a digital marketing company with many endeavors to promote others. I learned a lot of lessons from my experience there making their websites: what it is like to work for a start up company, working with real world customers, expanding my abilities and pushing myself, and the list goes on and on and on.<br><br>This website is the sub site of their main site, but I believe it would serve better as their main home site. Working with templates and building off what they had before my appearance in their company, I was able to improve the website ten fold!<br><br>I have built several other sites for this company and their clients, but I believe this site does is the best work I've given them that has been made live!"
     }
   },
@@ -31,7 +31,7 @@ export const previewDB = {
       "previewPath": "pages/preview.html?id=03",
       "sitePath":"youtube.com",
       "year":2022,
-      "carouselImgs" :["assets/resources/imgs/placeholder.jpg","assets/resources/imgs/placeholder.jpg","assets/resources/imgs/placeholder.jpg","assets/resources/imgs/placeholder.jpg","assets/resources/imgs/placeholder.jpg"],
+      "carouselImgs" :["assets/resources/imgs/previewPics/DifferenceAutoDetailing/Diff1.webp","assets/resources/imgs/previewPics/DifferenceAutoDetailing/Diff2.webp","assets/resources/imgs/previewPics/DifferenceAutoDetailing/Diff3.webp","assets/resources/imgs/previewPics/DifferenceAutoDetailing/Diff4.webp","assets/resources/imgs/previewPics/DifferenceAutoDetailing/Diff5.webp"],
       "description" : ""
     }
   },
